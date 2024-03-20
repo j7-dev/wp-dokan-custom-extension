134c99450f463db59c34916162314feae3230134
J7 <j7.dev.gg@outlook.com>
2024-03-21 01:33:34 +0800

    feat: add paynow info

--- inc/class/class-bootstrap.php
+++ inc/class/class-bootstrap.php
@@ -19,7 +19,8 @@ final class Bootstrap {
 	 * Constructor.
 	 */
 	public function __construct() {
-		\add_action( 'wp_enqueue_scripts', [ $this, 'load_scripts' ] );
+		\add_action( 'wp_enqueue_scripts', array( $this, 'load_scripts' ) );
+		\add_action( 'dokan_order_detail_after_order_general_details', array( $this, 'add_paynow_metabox' ) );
 	}
 
 	/**
@@ -29,14 +30,34 @@ final class Bootstrap {
 		\wp_enqueue_script(
 			Utils::KEBAB,
 			Utils::get_plugin_url() . '/js/dist/index.js',
-			[ 'jquery' ],
+			array( 'jquery' ),
 			Utils::get_plugin_ver(),
-			[
+			array(
 				'strategy'  => 'async',
 				'in_footer' => true,
-			]
+			)
 		);
 
-		\wp_enqueue_style( Utils::KEBAB, Utils::get_plugin_url() . '/js/dist/assets/css/index.css', [], Utils::get_plugin_ver(), 'all' );
+		\wp_enqueue_style( Utils::KEBAB, Utils::get_plugin_url() . '/js/dist/assets/css/index.css', array(), Utils::get_plugin_ver(), 'all' );
+	}
+
+	/**
+	 * Add pay now metabox
+	 */
+	public function add_paynow_metabox( $order ) {
+
+		$parent_order_id = \wp_get_post_parent_id( $order->get_id() );
+		if ( ! $parent_order_id ) {
+			return;
+		}
+
+		$order = \wc_get_order( $parent_order_id );
+
+		if ( class_exists( '\PayNow_Shipping_Order_Meta_Box' ) && method_exists( '\PayNow_Shipping_Order_Meta_Box', 'output' ) ) {
+			echo '<div class="dokan-panel dokan-panel-default">';
+			echo '<div class="dokan-panel-heading"><strong>Paynow 物流資訊</strong></div>';
+			\PayNow_Shipping_Order_Meta_Box::output( $order );
+			echo '</div>';
+		}
 	}
 }
