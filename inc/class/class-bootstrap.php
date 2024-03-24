4ea72a9297c750479f7cce2f7128e7769d8c42d6
J7 <j7.dev.gg@outlook.com>
2024-03-24 12:33:19 +0800

    feat: change hook to paynow_shipping_order_created

--- inc/class/class-bootstrap.php
+++ inc/class/class-bootstrap.php
@@ -21,7 +21,7 @@ final class Bootstrap {
 	public function __construct() {
 		\add_action( 'wp_enqueue_scripts', array( $this, 'load_scripts' ) );
 		\add_action( 'dokan_order_detail_after_order_general_details', array( $this, 'add_paynow_metabox' ) );
-		\add_action( 'dokan_create_sub_order_before_calculate_totals', array( $this, 'sync_sub_order_meta' ), 100, 2 );
+		\add_action( 'paynow_shipping_order_created', array( $this, 'sync_sub_order_meta' ), 100, 1 );
 	}
 
 	/**
@@ -77,7 +77,7 @@ final class Bootstrap {
 	 *
 	 * @return void
 	 */
-	public function sync_sub_order_meta( $order, $parent_order ): void {
+	public function do_sync_sub_order_meta( $order, $parent_order ): void {
 		$_paynow_shipping_logistic_service   = $parent_order->get_meta( '_paynow_shipping_logistic_service' );
 		$_paynow_shipping_logistic_number    = $parent_order->get_meta( '_paynow_shipping_logistic_number' );
 		$_paynow_shipping_paymentno          = $parent_order->get_meta( '_paynow_shipping_paymentno' );
@@ -94,6 +94,28 @@ final class Bootstrap {
 		$_shipping_paynow_storeid      = $parent_order->get_meta( '_shipping_paynow_storeid' );
 		$_shipping_paynow_storename    = $parent_order->get_meta( '_shipping_paynow_storename' );
 		$_shipping_phone               = $parent_order->get_meta( '_shipping_phone' );
+
+		ob_start();
+		print_r(
+			array(
+				'_paynow_shipping_logistic_service'   => $_paynow_shipping_logistic_service,
+				'_paynow_shipping_logistic_number'    => $_paynow_shipping_logistic_number,
+				'_paynow_shipping_paymentno'          => $_paynow_shipping_paymentno,
+				'_paynow_shipping_validation_no'      => $_paynow_shipping_validation_no,
+				'_paynow_shipping_return_msg'         => $_paynow_shipping_return_msg,
+				'_paynow_shipping_status'             => $_paynow_shipping_status,
+				'_paynow_shipping_logistic_sno'       => $_paynow_shipping_logistic_sno,
+				'_paynow_shipping_delivery_status'    => $_paynow_shipping_delivery_status,
+				'_paynow_shipping_logistic_code'      => $_paynow_shipping_logistic_code,
+				'_paynow_shipping_detail_status_desc' => $_paynow_shipping_detail_status_desc,
+				'_paynow_shipping_status_update_at'   => $_paynow_shipping_status_update_at,
+				'_shipping_paynow_storeaddress'       => $_shipping_paynow_storeaddress,
+				'_shipping_paynow_storeid'            => $_shipping_paynow_storeid,
+				'_shipping_paynow_storename'          => $_shipping_paynow_storename,
+				'_shipping_phone'                     => $_shipping_phone,
+			)
+		);
+		\J7\WpToolkit\Utils::debug_log( '' . ob_get_clean() );
 
 		$order->update_meta_data( '_paynow_shipping_logistic_service', $_paynow_shipping_logistic_service );
 		$order->update_meta_data( '_paynow_shipping_logistic_number', $_paynow_shipping_logistic_number );
@@ -113,5 +135,44 @@ final class Bootstrap {
 		$order->update_meta_data( '_shipping_phone', $_shipping_phone );
 
 		$order->save();
+	}
+
+	/**
+	 * Fires when order status is changed.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param WC_Order $parent_order $order Order object.
+	 */
+	public function sync_sub_order_meta( $parent_order ) {
+
+		$parent_order_id = $parent_order->get_id();
+		if ( ! $parent_order_id ) {
+			return;
+		}
+
+		$sub_order_ids = \get_children(
+			array(
+				'post_parent' => $parent_order_id,
+				'post_type'   => 'shop_order',
+				'post_status' => array( 'wc-pending', 'wc-processing', 'wc-completed' ),
+				'fields'      => 'ids',
+			)
+		);
+		if ( empty( $sub_order_ids ) ) {
+			return;
+		}
+
+		ob_start();
+		print_r( $sub_order_ids );
+		\J7\WpToolkit\Utils::debug_log( 'sub_order_ids ' . ob_get_clean() );
+
+		foreach ( $sub_order_ids as $order_id ) {
+			$order = \wc_get_order( $order_id );
+			if ( ! $order ) {
+				continue;
+			}
+			$this->do_sync_sub_order_meta( $order, $parent_order );
+		}
 	}
 }
